f8c0eed4491149444b8b2e69b652cf5aae835a75
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-05-19 23:17:21 +0800

    将函数合并成一个类

--- scripts/src/yoni/ymfs/fs/YmInternalFS.ts
+++ scripts/src/yoni/ymfs/fs/YmInternalFS.ts
@@ -46,142 +46,19 @@ class VolumeUtils {
 */
 import { Volume } from "../disk/Volume.js";
 
-export function isVolumeHasInitializedYmFS(volume: Volume): boolean {
-    return volume.getChunk(0).getDataStr(0) === "Yoni_YmFS\u00001";
-}
-export function readVBlockStats(volume: Volume): number[] {
-    return volume.getChunk(0).getDataStr(1).split("").map(Number);
-}
-export function writeVBlockStats(volume: Volume, stats: number[]){
-    volume.getChunk(0).setDataStr(1, stats.join(""));
-}
-let vblockStats: number[];
-export function allocateVBlocks(volume: Volume, count: number){
-    let vblocks: number[] = [];
-    let lastSelectedIndex = -1;
-    
-    for (let _i = 0; _i < vblockStats.length; _i ++){
-        if (vblockStats[_i] === VBlockTypes.Deleted
-        || vblockStats[_i] === VBlockTypes.Empty){
-            vblocks.push(_i);
-        }
-        if (count <= vblocks.length){
-            break;
-        }
-    }
-    if (count > vblocks.length){
-        throw new Error();
-    }
-    
-    cleanVBlocks(volume, vblocks);
-    return vblocks;
+/** 区块状态枚举 */
+export enum VBlockTypes {
+    Empty = 0,
+    Super = 1,
+    Data = 2,
+    Normal = 3,
+    Deleted = 4,
 }
 
-export function createDataBlocks(volume: Volume, count: number){
-    let vblocks = allocateVBlocks(volume, count);
-    for (let idx of vblocks){
-        vblockStats[idx] = VBlockTypes.Data;
-    }
-}
-export function createNodeBlock(volume: Volume){
-    let [vblock] = allocateVBlocks(volume, 1);
-    vblockStats[vblock] = VBlockTypes.Normal;
-    let time = Date.now();
-    writeNodeInfo(volume, vblock, {
-        curIndex: vblock,
-        fileType: FileType.dummy,
-        fileSize: 0,
-        linkCount: 0,
-        createTime: time,
-        modifyTime: time,
-        accessTime: time,
-    });
-    return vblock;
-}
-enum FileType {
-    directory = "d",
-    file = "f",
-    symlink = "s",
-    dummy = "0",
-}
-export function modFileType(volume: Volume, nodeBlock: number, type: FileType){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    let info = readNodeInfo(volume, nodeBlock);
-    info.fileType = type;
-    writeNodeInfo(volume, nodeBlock, info);
-}
-export function getFileSize(volume: Volume, nodeBlock: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    let info = readNodeInfo(volume, nodeBlock);
-    return info.fileSize;
-}
-export function modFileSize(volume: Volume, nodeBlock: number, size: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    if (size < 0 || Math.floor(size) !== size || !isFinite(size)){
-        throw new Error();
-    }
-    let info = readNodeInfo(volume, nodeBlock);
-    info.fileSize = size;
-    writeNodeInfo(volume, nodeBlock, info);
-}
-export function increaseLinkCount(volume: Volume, nodeBlock: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    let info = readNodeInfo(volume, nodeBlock);
-    info.linkCount += 1;
-    writeNodeInfo(volume, nodeBlock, info);
-}
-export function getVBlockStat(vblock: number): VBlockTypes {
-    return vblockStats[vblock] as unknown as VBlockTypes;
-}
-export function atime(volume: Volume, nodeBlock: number, time: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    writeNodeInfo(volume, nodeBlock, Object.assign(readNodeInfo(volume, nodeBlock), { accessTime: time}));
-}
-export function ctime(volume: Volume, nodeBlock: number, time: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    writeNodeInfo(volume, nodeBlock, Object.assign(readNodeInfo(volume, nodeBlock), { createTime: time}));
-}
-export function mtime(volume: Volume, nodeBlock: number, time: number){
-    if (getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
-        throw new Error();
-    }
-    writeNodeInfo(volume, nodeBlock, Object.assign(readNodeInfo(volume, nodeBlock), { modifyTime: time}));
-}
-function isValidFileType(v: any): v is FileType {
-    return Object.values(FileType).some(t => t === v);
-}
-export function readNodeInfo(volume: Volume, nodeBlock: number): NodeVBlockInfo {
-    let infoArr = volume.getChunk(nodeBlock).getStringArray(0);
-    let [curindex, filetype, fileSize, linkCount, createTime, modifyTime, accessTime] = infoArr;
-    let r_filetype: FileType;
-    if (isValidFileType(filetype)){
-        r_filetype = filetype;
-    } else {
-        throw new Error();
-    }
-    return {
-        curIndex: Number(curindex),
-        fileType: r_filetype,
-        fileSize: Number(fileSize),
-        linkCount: Number(linkCount),
-        createTime: Number(createTime),
-        modifyTime: Number(modifyTime),
-        accessTime: Number(accessTime),
-    };
-}
-interface NodeVBlockInfo {
+export type DataBlockInfo = any;
+export type VBlockStats = VBlockTypes[];
+export type VBlockIndex = number;
+export interface NodeVBlockInfo {
     curIndex: number;
     fileType: FileType;
     fileSize: number;
@@ -190,73 +67,280 @@ interface NodeVBlockInfo {
     modifyTime: number;
     accessTime: number;
 }
-export function writeNodeInfo(volume: Volume, nodeBlock: number, info: Partial<NodeVBlockInfo>){
-    let infoArr = volume.getChunk(nodeBlock).getStringArray(0);
-    let [curindex_old, filetype_old, fileSize_old, linkCount_old, createTime_old, modifyTime_old, accessTime_old] = infoArr;
-    let info2 = Object.assign({
-        curIndex: Number(curindex_old),
-        fileType: filetype_old,
-        fileSize: Number(fileSize_old),
-        likeCount: Number(linkCount_old),
-        createTime: Number(createTime_old),
-        modifyTime: Number(modifyTime_old),
-        accessTime: Number(accessTime_old),
-    }, info);
-    let { curIndex, fileType, fileSize, likeCount, createTime, modifyTime, accessTime } = info2;
-    
-    let info2Arr = [curIndex, fileType, fileSize, likeCount, createTime, modifyTime, accessTime];
-    volume.getChunk(nodeBlock).setStringArray(0, info2Arr.map(String));
+
+export enum FileType {
+    directory = "d",
+    file = "f",
+    symlink = "s",
+    dummy = "0",
 }
-export function createDir(volume: Volume, parent: number){
-    let nodeBlock = createNodeBlock(volume);
-    let vblock = volume.getChunk(nodeBlock);
-    modFileType(volume, nodeBlock, FileType.directory);
-}
-export function createFile(volume: Volume){
-    let nodeBlock = createNodeBlock(volume);
-    let vblock = volume.getChunk(nodeBlock);
-    modFileType(volume, nodeBlock, FileType.file);
-}
-enum ErrorCode {
-}
-//export function readDataBlock(volume: Volume, vblock: 
-export function readFile(volume: Volume, fileNode: number, startIndex: number, length: number): ArrayBuffer | -1 {
-    let dataBlocks = volume.getChunk(fileNode).getDataStr(1).split(".").map(Number);
-    let maxSize = getFileSize(volume, fileNode);
-    if (maxSize < startIndex + length || length < 0){
-        return -1;
+
+export class InternalVolumeFileSystem {
+    static isVolumeHasInitializedYmFS(volume: Volume): boolean {
+        return volume.getChunk(0)
+            .getDataStr(0) === "Yoni_YmFS\u00001";
     }
-    let buffer = new ArrayBuffer(length);
-    let startDataBlock = Math.floor(startIndex / 512);
-    let dataStartIndex = startIndex % 512;
-    for (let i = startDataBlock; i < dataBlocks.length; i++){
+    vBlockStats: VBlockStats;
+    static readVBlockStats(volume: Volume): VBlockStats {
+        const stats = volume.getChunk(0)
+            .getDataStr(1)
+            .split("")
+            .map(Number);
+        if (!InternalVolumeFileSystem.#checkVBlockStats(stats)){
+            throw new Error();
+        }
+        return stats;
+    }
+    static #checkVBlockStats(unknownStats: number[]): unknownStats is VBlockStats {
+        const values = Object.values(VBlockTypes);
+        for (const ustastval of unknownStats){
+            if (!values.includes(ustastval)){
+                return false;
+            }
+        }
+        return true;
+    }
+    static writeVBlockStats(volume: Volume, stats: VBlockStats){
+        volume.getChunk(0).setDataStr(1, stats.join(""));
+    }
+    getVBlockStat(vblock: number): VBlockTypes {
+        const stat = this.vBlockStats[vblock];
+        if (stat == null){
+            throw new Error();
+        }
+        return stat;
+    }
+    volume: Volume;
+    allocateVBlocks(count: number): VBlockIndex[] {
+        let vblocks: VBlockIndex[] = [];
         
+        for (let _i = 0; _i < this.vBlockStats.length; _i ++){
+            if (this.vBlockStats[_i] === VBlockTypes.Deleted
+            || this.vBlockStats[_i] === VBlockTypes.Empty){
+                vblocks.push(_i);
+            }
+            if (count <= vblocks.length){
+                break;
+            }
+        }
+        if (count > vblocks.length){
+            throw new Error();
+        }
+        
+        this.cleanVBlocks(vblocks);
+        return vblocks;
     }
-    return buffer;
-}
-
-export function writeFile(){
-}
-
-export function listDir(volume: Volume, block: number){
-}
-export function deleteFile(){
-}
-
-export function cleanVBlocks(volume: Volume, vblocks: number[]){
-    for (let idx of vblocks){
-        let chunk = volume.getChunk(idx);
-        for (let b = 0; b < chunk.size; b++){
-            chunk.setDataStr(b, "");
+    cleanVBlocks(vblocks: number[]){
+        for (let idx of vblocks){
+            let chunk = this.volume.getChunk(idx);
+            for (let b = 0; b < chunk.size; b++){
+                chunk.setDataStr(b, "");
+            }
         }
     }
-}
+    createDataBlocks(count: number){
+        let vblocks = this.allocateVBlocks(count);
+        for (let idx of vblocks){
+            this.vBlockStats[idx] = VBlockTypes.Data;
+        }
+    }
+    dataBlockStat(dbblock: number): DataBlockInfo {
+    }
+    readDataBlock(dbblock: number, start: number, length: number): ArrayBuffer {
+    }
+    
+    createNodeBlock(){
+        let [vblock] = this.allocateVBlocks(1);
+        this.vBlockStats[vblock] = VBlockTypes.Normal;
+        let time = Date.now();
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, vblock, {
+            curIndex: vblock,
+            fileType: FileType.dummy,
+            fileSize: 0,
+            linkCount: 0,
+            createTime: time,
+            modifyTime: time,
+            accessTime: time,
+        });
+        return vblock;
+    }
+    
+    static readNodeInfo(volume: Volume, nodeBlock: number): NodeVBlockInfo {
+        let infoArr = volume.getChunk(nodeBlock).getStringArray(0);
+        let [curindex, filetype, fileSize, linkCount, createTime, modifyTime, accessTime] = infoArr;
+        let r_filetype: FileType;
+        if (InternalVolumeFileSystem.isValidFileType(filetype)){
+            r_filetype = filetype;
+        } else {
+            throw new Error();
+        }
+        return {
+            curIndex: Number(curindex),
+            fileType: r_filetype,
+            fileSize: Number(fileSize),
+            linkCount: Number(linkCount),
+            createTime: Number(createTime),
+            modifyTime: Number(modifyTime),
+            accessTime: Number(accessTime),
+        };
+    }
+    static writeNodeInfo(volume: Volume, nodeBlock: number, info: Partial<NodeVBlockInfo>){
+        let infoArr = volume.getChunk(nodeBlock).getStringArray(0);
+        let [curindex_old, filetype_old, fileSize_old, linkCount_old, createTime_old, modifyTime_old, accessTime_old] = infoArr;
+        let info2 = Object.assign({
+            curIndex: Number(curindex_old),
+            fileType: filetype_old,
+            fileSize: Number(fileSize_old),
+            likeCount: Number(linkCount_old),
+            createTime: Number(createTime_old),
+            modifyTime: Number(modifyTime_old),
+            accessTime: Number(accessTime_old),
+        }, info);
+        let { curIndex, fileType, fileSize, likeCount, createTime, modifyTime, accessTime } = info2;
+        
+        let info2Arr = [curIndex, fileType, fileSize, likeCount, createTime, modifyTime, accessTime];
+        volume.getChunk(nodeBlock).setStringArray(0, info2Arr.map(String));
+    }
+    
+    static #validFileTypes = Object.freeze(Object.values(FileType));
+    static isValidFileType(v: any): v is FileType {
+        return InternalVolumeFileSystem.#validFileTypes.some(t => t === v);
+    }
+    
+    getFileType(nodeBlock: number): FileType {
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        let { fileType } = info;
+        if (InternalVolumeFileSystem.isValidFileType(info.fileType)){
+            return fileType;
+        } else {
+            throw new Error();
+        }
+    }
+    modFileType(nodeBlock: number, type: FileType){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        info.fileType = type;
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, info);
+    }
+    
+    getFileSize(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        return info.fileSize;
+    }
+    modFileSize(nodeBlock: number, size: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        if (size < 0 || Math.floor(size) !== size || !isFinite(size)){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        info.fileSize = size;
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, info);
+    }
+    
+    getLinkCount(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        return info.linkCount;
+    }
+    increaseLinkCount(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        info.linkCount += 1;
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, info);
+    }
+    decreaseLinkCount(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        let info = InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock);
+        info.linkCount -= 1;
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, info);
+    }
+    
+    getatime(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        return InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock).accessTime;
+    }
+    atime(nodeBlock: number, time: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, Object.assign(InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock), { accessTime: time}));
+    }
+    
+    getctime(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        return InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock).createTime;
+    }
+    ctime(nodeBlock: number, time: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, Object.assign(InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock), { createTime: time}));
+    }
+    
+    getmtime(nodeBlock: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        return InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock).modifyTime;
+    }
+    mtime(nodeBlock: number, time: number){
+        if (this.getVBlockStat(nodeBlock) !== VBlockTypes.Normal){
+            throw new Error();
+        }
+        InternalVolumeFileSystem.writeNodeInfo(this.volume, nodeBlock, Object.assign(InternalVolumeFileSystem.readNodeInfo(this.volume, nodeBlock), { modifyTime: time}));
+    }
+    
+    createDir(parent: number){
+        let nodeBlock = this.createNodeBlock();
+        let vblock = this.volume.getChunk(nodeBlock);
+        this.modFileType(nodeBlock, FileType.directory);
+    }
+    createFile(volume: Volume){
+        let nodeBlock = this.createNodeBlock();
+        let vblock = volume.getChunk(nodeBlock);
+        this.modFileType(nodeBlock, FileType.file);
+    }
 
-/** 区块状态枚举 */
-export enum VBlockTypes {
-    Empty = 0,
-    Super = 1,
-    Data = 2,
-    Normal = 3,
-    Deleted = 4,
+    readFile(fileNode: number, startIndex: number, length: number): ArrayBuffer | -1 {
+        let dataBlocks = this.volume.getChunk(fileNode).getDataStr(1).split(".").map(Number);
+        let maxSize = this.getFileSize(fileNode);
+        if (maxSize < startIndex + length || length < 0){
+            return -1;
+        }
+        let buffer = new ArrayBuffer(length);
+        let startDataBlock = Math.floor(startIndex / 512);
+        let dataStartIndex = startIndex % 512;
+        for (let i = startDataBlock; i < dataBlocks.length; i++){
+            
+        }
+        return buffer;
+    }
+    
+    writeFile(){
+    }
+    
+    listDir(volume: Volume, block: number){
+    }
+    deleteFile(){
+    }
 }
