a9ad4a4eaa511cf6c7fcadf111683a09fe00e6b4
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-06-11 20:19:34 +0800

    更新0.9.1

---
+++ install_deps.sh
@@ -0,0 +1,4 @@
+npm i @minecraft/server@1.2.0-beta.1.19.80-stable
+npm i @minecraft/server-ui@1.0.0-beta.1.19.80-stable
+npm i @minecraft/server-gametest@1.0.0-beta.1.19.80-stable
+npm i @minecraft/vanilla-data@1.19.80
