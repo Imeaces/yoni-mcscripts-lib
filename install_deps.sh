708de295bf1718ef2975b3c074d47b131efa21e4
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-07-30 10:30:18 +0800

    使用正式版中的betaapi@

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,2 @@
-npm i --no-bin-links @minecraft/server@1.4.0-beta.1.20.10-preview.21
-npm i --no-bin-links @minecraft/server-ui@1.1.0-beta.1.20.10-preview.21
-npm i --no-bin-links @minecraft/server-gametest@1.0.0-beta.1.20.10-preview.21
-npm i --no-bin-links @minecraft/vanilla-data@1.20.10-preview.21
+npm i --no-bin-links @minecraft/server@1.4.0-beta.1.20.10-stable
+npm i --no-bin-links @minecraft/server-gametest@1.0.0-beta.1.20.10-stable
