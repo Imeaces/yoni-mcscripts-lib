37f619b4c9d0591dd384eb3fd4721f0cfa6e0b51
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2023-09-28 16:56:50 +0800

    update version b1.20.40.22

--- install_deps.sh
+++ install_deps.sh
@@ -1,2 +1,2 @@
-npm i --no-bin-links @minecraft/server@1.6.0-beta.1.20.30-stable
-npm i --no-bin-links @minecraft/server-gametest@1.0.0-beta.1.20.30-stable
+npm i --no-bin-links @minecraft/server@1.7.0-beta.1.20.40-preview.22
+npm i --no-bin-links @minecraft/server-gametest@1.0.0-beta.1.20.40-preview.22
